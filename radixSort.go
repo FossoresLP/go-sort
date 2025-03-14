3279b859ef5e9843cd06ae597a1ccac6f1c14e19
Pascal Vorwerk <info@fossores.de>
2025-03-14 20:20:13 +0100

    fix: reduce memory usage of radix sort

--- radixSort.go
+++ radixSort.go
@@ -1,28 +1,18 @@
 package sort
 
-import (
-	"encoding/binary"
-)
-
-// RadixSort is radix sort for 64 bit unsigned integers implemented using binary data.
-// This algorithm does not compare the data and due to the binary implementation also does not perform any calculations with it.
-// It is faster than quicksort but cannot handle negative values.
+// RadixSort is radix sort for 64 bit unsigned integers
+// It operates byte-by-byte using 256 buckets
 func RadixSort(items []uint64) []uint64 {
-	// Create variables
-	size := len(items)
-	sort := make([][8]byte, size)
-	sorted := make([][8]byte, size)
-
-	// Convert the unsigned integers to binary data
-	for i := 0; i < size; i++ {
-		binary.LittleEndian.PutUint64(sort[i][:], items[i])
-	}
+	l := len(items)
+	tmp := make([]uint64, l)
+	src := items
+	dst := tmp
 
 	// Loop over the 8 bytes created for each unsigned integer
-	for e := 0; e < 8; e++ {
-		bucket := [256]int{0}
-		for i := 0; i < size; i++ {
-			bucket[sort[i][e]]++
+	for shift := 0; shift < 64; shift += 8 {
+		bucket := [256]int{}
+		for i := 0; i < l; i++ {
+			bucket[int(src[i]>>shift&0xFF)]++
 		}
 
 		// Add count from previous bucket
@@ -32,19 +22,15 @@ func RadixSort(items []uint64) []uint64 {
 		}
 
 		// Use the buckets indices when filling the sorted array
-		for i := size - 1; i >= 0; i-- {
-			bucket[sort[i][e]]--
-			sorted[bucket[sort[i][e]]] = sort[i]
+		for i := l - 1; i >= 0; i-- {
+			v := &bucket[int(src[i]>>shift&0xFF)]
+			*v--
+			dst[*v] = src[i]
 		}
 
-		// Copy the values from sorted back to sort for the next run of the loop
-		copy(sort, sorted)
+		// Swap source and destination for the next pass
+		src, dst = dst, src
 	}
 
-	// Convert the bytes back to unsigned integers and return those
-	out := make([]uint64, size)
-	for i := 0; i < size; i++ {
-		out[i] = binary.LittleEndian.Uint64(sorted[i][:])
-	}
-	return out
+	return items
 }
