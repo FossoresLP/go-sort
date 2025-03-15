52603851c09c3b4f6d842117cb7fde1ca0ab9999
Pascal Vorwerk <info@fossores.de>
2025-03-15 17:35:03 +0100

    deps: switch tests to slices.Sort

--- compare_test.go
+++ compare_test.go
@@ -3,21 +3,9 @@ package sort
 import (
 	"math/rand"
 	"reflect"
-	"sort"
+	"slices"
 	"testing"
 )
-
-type int64arr []int64
-
-func (a int64arr) Len() int           { return len(a) }
-func (a int64arr) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a int64arr) Less(i, j int) bool { return a[i] < a[j] }
-
-type uint64arr []uint64
-
-func (a uint64arr) Len() int           { return len(a) }
-func (a uint64arr) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a uint64arr) Less(i, j int) bool { return a[i] < a[j] }
 
 func TestInsertionSort(t *testing.T) {
 	values := make([]int64, 1000)
@@ -27,9 +15,9 @@ func TestInsertionSort(t *testing.T) {
 	values2 := make([]int64, 1000)
 	copy(values2, values)
 	InsertionSort(values)
-	sort.Sort(int64arr(values2))
+	slices.Sort(values2)
 	if !reflect.DeepEqual(values, values2) {
-		t.Error("InsertionSort does not produce the same output as go's sort package.")
+		t.Error("InsertionSort does not produce the same output as slices.Sort.")
 	}
 }
 
@@ -41,9 +29,9 @@ func TestMergeSort(t *testing.T) {
 	values2 := make([]int64, 1000)
 	copy(values2, values)
 	values = MergeSort(values)
-	sort.Sort(int64arr(values2))
+	slices.Sort(values2)
 	if !reflect.DeepEqual(values, values2) {
-		t.Error("MergeSort does not produce the same output as go's sort package.")
+		t.Error("MergeSort does not produce the same output as slices.Sort.")
 	}
 }
 
@@ -55,9 +43,9 @@ func TestQuickSort(t *testing.T) {
 	values2 := make([]int64, 1000)
 	copy(values2, values)
 	QuickSort(values)
-	sort.Sort(int64arr(values2))
+	slices.Sort(values2)
 	if !reflect.DeepEqual(values, values2) {
-		t.Error("QuickSort does not produce the same output as go's sort package.")
+		t.Error("QuickSort does not produce the same output as slices.Sort.")
 	}
 }
 
@@ -69,8 +57,8 @@ func TestRadixSort(t *testing.T) {
 	values2 := make([]uint64, 1000)
 	copy(values2, values)
 	values = RadixSort(values)
-	sort.Sort(uint64arr(values2))
+	slices.Sort(values2)
 	if !reflect.DeepEqual(values, values2) {
-		t.Error("RadixSort does not produce the same output as go's sort package.")
+		t.Error("RadixSort does not produce the same output as slices.Sort.")
 	}
 }
