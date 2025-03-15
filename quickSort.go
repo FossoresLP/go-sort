067b7566d5ba6ecbf1dabf7aa3a92d2b81a0bb95
Pascal Vorwerk <info@fossores.de>
2025-03-15 17:02:56 +0100

    feat: simplify and deprecate quicksort in favour of slices.Sort

--- quickSort.go
+++ quickSort.go
@@ -2,40 +2,32 @@ package sort
 
 import "cmp"
 
-// QuickSort sorts the supplied integer array using quicksort
-func QuickSort[T cmp.Ordered](s []T) []T {
-	quicksort(&s, 0, len(s)-1)
-	return s
-}
-
-func quicksort[T cmp.Ordered](s *[]T, lo, hi int) {
-	if lo < hi {
-		p := partition(s, lo, hi)
-		quicksort(s, lo, p)
-		quicksort(s, p+1, hi)
+// QuickSort implements the quicksort algorithm for all ordered primitive types.
+// It operates in-place without additional memory allocations.
+//
+// Deprecated: use slices.Sort instead which provides a more optimized implementation of quicksort.
+func QuickSort[T cmp.Ordered](items []T) []T {
+	if len(items) < 2 {
+		return items
 	}
-}
-
-func partition[T cmp.Ordered](s *[]T, lo, hi int) int {
-	pivot := (*s)[lo]
-	i := lo - 1
-	if i == -1 {
-		i++
-	}
-	j := hi + 1
-	if j == len(*s) {
-		j--
-	}
+	pivot := items[0]
+	i := 0
+	j := len(items) - 1
+	p := 0
 	for {
-		for (*s)[i] < pivot {
+		for items[i] < pivot {
 			i++
 		}
-		for (*s)[j] > pivot {
+		for items[j] > pivot {
 			j--
 		}
 		if i >= j {
-			return j
+			p = j
+			break
 		}
-		(*s)[i], (*s)[j] = (*s)[j], (*s)[i]
+		items[i], items[j] = items[j], items[i]
 	}
+	QuickSort(items[:p])
+	QuickSort(items[p+1:])
+	return items
 }
